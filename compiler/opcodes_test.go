ef951b9c80035023be81693ba7f1136252d63011
Daniel Kang <me@daniel.gs>
2019-01-16 12:22:25 -0800

    move opcodes code

--- compiler/opcodes_test.go
+++ compiler/opcodes_test.go
@@ -13,9 +13,8 @@ func TestReadOperands(t *testing.T) {
 
 func assertReadOperand(t *testing.T, opcode compiler.Opcode, operands []int, expectedBytes int) {
 	inst := compiler.MakeInstruction(opcode, operands...)
-	def, ok := compiler.Lookup(opcode)
-	assert.True(t, ok)
-	operandsRead, read := compiler.ReadOperands(def, inst[1:])
+	numOperands := compiler.OpcodeOperands[opcode]
+	operandsRead, read := compiler.ReadOperands(numOperands, inst[1:])
 	assert.Equal(t, expectedBytes, read)
 	assert.Equal(t, operands, operandsRead)
 }
