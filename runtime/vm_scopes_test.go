65c4a040e631f24496d5e8c84f9c15b4831b5402
Daniel Kang <me@daniel.gs>
2019-03-23 15:51:35 -0700

    add some more tests

--- runtime/vm_scopes_test.go
+++ runtime/vm_scopes_test.go
@@ -3,15 +3,28 @@ package runtime_test
 import "testing"
 
 func TestVMScopes(t *testing.T) {
-	// 'c' in outer scope
+	// shadowed global variable
 	expect(t, `
 c := 5
 if a := 3; a {
-	c := 6 // shadowed
+	c := 6
 } else {
-	c := 7 // shadowed
+	c := 7
 }
 out = c
+`, nil, 5)
+
+	// shadowed local variable
+	expect(t, `
+func() {
+	c := 5
+	if a := 3; a {
+		c := 6
+	} else {
+		c := 7
+	}
+	out = c
+}()
 `, nil, 5)
 
 	// 'b' is declared in 2 separate blocks
@@ -27,9 +40,10 @@ if a := 3; a {
 out = c
 `, nil, 8)
 
-	//
+	// shadowing inside for statement
 	expect(t, `
-a := 5
+a := 4
+b := 5
 for i:=0;i<3;i++ {
 	b := 6
 	for j:=0;j<2;j++ {
@@ -38,4 +52,52 @@ for i:=0;i<3;i++ {
 	}
 }
 out = a`, nil, 2)
+
+	// shadowing variable declared in init statement
+	expect(t, `
+if a := 5; a {
+	a := 6
+	out = a
+}`, nil, 6)
+	expect(t, `
+a := 4
+if a := 5; a {
+	a := 6
+	out = a
+}`, nil, 6)
+	expect(t, `
+a := 4
+if a := 0; a {
+	a := 6
+	out = a
+} else {
+	a := 7
+	out = a
+}`, nil, 7)
+	expect(t, `
+a := 4
+if a := 0; a {
+	out = a
+} else {
+	out = a
+}`, nil, 0)
+
+	// shadowing function level
+	expect(t, `
+a := 5
+func() {
+	a := 6
+	a = 7
+}()
+out = a
+`, nil, 5)
+	expect(t, `
+a := 5
+func() {
+	if a := 7; true {
+		a = 8
+	}
+}()
+out = a
+`, nil, 5)
 }
