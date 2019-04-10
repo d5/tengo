a4b2ebb2380c16978b01358dfb1e1b21fe94be84
Earncef Sequeira <earncef@earncef.com>
2019-04-10 13:57:03 +0200

    Added format (alias for fmt.sprintf) to builtins.

---
+++ objects/builtin_format.go
@@ -0,0 +1,44 @@
+package objects
+
+import (
+	"fmt"
+
+	"github.com/d5/tengo"
+)
+
+func builtinFormat(args ...Object) (Object, error) {
+	numArgs := len(args)
+	if numArgs == 0 {
+		return nil, ErrWrongNumArguments
+	}
+
+	format, ok := args[0].(*String)
+	if !ok {
+		return nil, ErrInvalidArgumentType{
+			Name:     "format",
+			Expected: "string",
+			Found:    args[0].TypeName(),
+		}
+	}
+	if numArgs == 1 {
+		return format, nil // okay to return 'format' directly as String is immutable
+	}
+
+	formatArgs := make([]interface{}, numArgs-1, numArgs-1)
+	for idx, arg := range args[1:] {
+		switch arg := arg.(type) {
+		case *Int, *Float, *Bool, *Char, *String, *Bytes:
+			formatArgs[idx] = ToInterface(arg)
+		default:
+			formatArgs[idx] = arg
+		}
+	}
+
+	s := fmt.Sprintf(format.Value, formatArgs...)
+
+	if len(s) > tengo.MaxStringLen {
+		return nil, ErrStringLimit
+	}
+
+	return &String{Value: s}, nil
+}
