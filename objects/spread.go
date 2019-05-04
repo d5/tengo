ca8457cc18467838ed231c3013e3a16a92223f3c
Jacob McCollum <jacob.r.mccollum@gmail.com>
2019-05-04 14:18:42 -0400

    fix lint errors

--- objects/spread.go
+++ objects/spread.go
@@ -1,9 +1,13 @@
 package objects
 
+// Spread represents a value that will been spread onto the stack by the VM
 type Spread struct {
 	ObjectImpl
 	Values []Object
 }
 
+// TypeName returns the name of the type.
 func (s *Spread) TypeName() string { return "spread-values" }
-func (s *Spread) String() string   { return "<" + s.TypeName() + ">" }
+
+// String returns a string representation of the type's value.
+func (s *Spread) String() string { return "<" + s.TypeName() + ">" }
