86fb0eb567667188d419fda913f46303971be044
Jacob McCollum <jacob.r.mccollum@gmail.com>
2019-07-05 11:17:40 -0400

    add compiler test for try syntax

--- compiler/compiler_try_test.go
+++ compiler/compiler_try_test.go
@@ -2,7 +2,48 @@ package compiler_test
 
 import (
 	"testing"
+
+	"github.com/d5/tengo/compiler"
 )
 
 func TestTry(t *testing.T) {
+	expect(t, `try(1)`, bytecode(
+		concat(
+			compiler.MakeInstruction(compiler.OpConstant, 0),
+			compiler.MakeInstruction(compiler.OpReturnOnError, 1),
+			compiler.MakeInstruction(compiler.OpPop),
+			compiler.MakeInstruction(compiler.OpSuspend),
+		),
+		objectsArray(
+			intObject(1),
+		),
+	))
+
+	expect(t, `try(1+1)`, bytecode(
+		concat(
+			compiler.MakeInstruction(compiler.OpConstant, 0),
+			compiler.MakeInstruction(compiler.OpConstant, 0),
+			compiler.MakeInstruction(compiler.OpBinaryOp, 11),
+			compiler.MakeInstruction(compiler.OpReturnOnError, 1),
+			compiler.MakeInstruction(compiler.OpPop),
+			compiler.MakeInstruction(compiler.OpSuspend),
+		),
+		objectsArray(
+			intObject(1),
+		),
+	))
+
+	expect(t, `x := try(1+1)`, bytecode(
+		concat(
+			compiler.MakeInstruction(compiler.OpConstant, 0),
+			compiler.MakeInstruction(compiler.OpConstant, 0),
+			compiler.MakeInstruction(compiler.OpBinaryOp, 11),
+			compiler.MakeInstruction(compiler.OpReturnOnError, 1),
+			compiler.MakeInstruction(compiler.OpSetGlobal, 0),
+			compiler.MakeInstruction(compiler.OpSuspend),
+		),
+		objectsArray(
+			intObject(1),
+		),
+	))
 }
