2c6f14694791e56e2a428130c99b3b281d77d95b
Daniel Robinson <daniel.robinson.open@gmail.com>
2021-07-27 10:18:27 +0100

    tweak http test

--- stdlib/http_test.go
+++ stdlib/http_test.go
@@ -9,11 +9,11 @@ import (
 )
 
 func TestHTTP(t *testing.T) {
-	run := func(method string, url string, headers string, body string, errMsg string) {
+	run := func(required string, opts string, errMsg string) {
 		script := tengo.NewScript([]byte(fmt.Sprintf(`
 http := import("http")
-res := http.do(%q, %q, %s, %s)
-`, method, url, headers, body)))
+res := http.do(%s%s)
+`, required, opts)))
 		script.SetImports(stdlib.GetModuleMap("http"))
 
 		executed, err := script.Run()
@@ -46,6 +46,8 @@ res := http.do(%q, %q, %s, %s)
 		check("body", func(i interface{}) bool { v, ok := i.([]byte); return ok && len(v) > 0 })
 	}
 
-	run(`GET`, `https://avatars.githubusercontent.com/u/1291934?s=48&v=4`, `{dnt: 1}`, `bytes("yolo")`, ``)
-	run(`GET`, `tengo`, `{dnt: 1}`, `bytes("yolo")`, `error: "Get \"tengo\": unsupported protocol scheme \"\""`)
+	required := `"GET", "https://avatars.githubusercontent.com/u/1291934?s=48&v=4"`
+	run(required, `, {dnt: 1, "my-header": "yolo"}, bytes("test")`, ``)
+	run(required, ``, ``)
+	run(`"GET", "tengo"`, ``, `error: "Get \"tengo\": unsupported protocol scheme \"\""`)
 }
