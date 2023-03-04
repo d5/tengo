e925584d2fa3950da7e0ad8978266106090a8a8c
Chanter Chen <51439154+sin3degrees@users.noreply.github.com>
2023-03-04 22:57:33 +0800

    Update main.go

--- cmd/tengo/main.go
+++ cmd/tengo/main.go
@@ -11,9 +11,9 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/d5/tengo/v2"
-	"github.com/d5/tengo/v2/parser"
-	"github.com/d5/tengo/v2/stdlib"
+	"github.com/sin3degrees/tengo/v2"
+	"github.com/sin3degrees/tengo/v2/parser"
+	"github.com/sin3degrees/tengo/v2/stdlib"
 )
 
 const (
