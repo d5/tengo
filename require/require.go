62f937b34652b8ab9bb829c2818794881b0c5af2
Chanter Chen <51439154+sin3degrees@users.noreply.github.com>
2023-03-04 23:06:37 +0800

    Update require.go

--- require/require.go
+++ require/require.go
@@ -10,9 +10,9 @@ import (
 	"unicode"
 	"unicode/utf8"
 
-	"github.com/d5/tengo/v2"
-	"github.com/d5/tengo/v2/parser"
-	"github.com/d5/tengo/v2/token"
+	"github.com/sin3degrees/tengo/v2"
+	"github.com/sin3degrees/tengo/v2/parser"
+	"github.com/sin3degrees/tengo/v2/token"
 )
 
 // NoError asserts err is not an error.
