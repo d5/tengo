3d79296cfa477d19a8b688df028e283a1b1e6542
Chanter Chen <51439154+sin3degrees@users.noreply.github.com>
2023-03-04 23:08:09 +0800

    Update json_test.go

--- stdlib/json/json_test.go
+++ stdlib/json/json_test.go
@@ -4,9 +4,9 @@ import (
 	gojson "encoding/json"
 	"testing"
 
-	"github.com/d5/tengo/v2"
-	"github.com/d5/tengo/v2/require"
-	"github.com/d5/tengo/v2/stdlib/json"
+	"github.com/sin3degrees/tengo/v2"
+	"github.com/sin3degrees/tengo/v2/require"
+	"github.com/sin3degrees/tengo/v2/stdlib/json"
 )
 
 type ARR = []interface{}
