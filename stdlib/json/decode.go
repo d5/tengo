e6b9d57609820a39bc67c36c85984857c72fc4cb
E Sequeira <5458743+geseq@users.noreply.github.com>
2023-06-25 16:54:43 +0100

    fix json decode

--- stdlib/json/decode.go
+++ stdlib/json/decode.go
@@ -65,7 +65,7 @@ func (d *decodeState) scanNext() {
 func (d *decodeState) scanWhile(op int) (isFloat bool) {
 	s, data, i := &d.scan, d.data, d.off
 	for i < len(data) {
-		if data[i] == '.' {
+		if !unicode.IsDigit(data) {
 			isFloat = true
 		}
 		newOp := s.step(s, data[i])
